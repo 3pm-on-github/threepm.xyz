6ff7084c7615603b13b3d72610d54d3322b953f4
3pm-on-github <pookiemon2019@gmail.com>
2025-01-12 10:53:18 +0100

    added an empty comment because i need to make another commit for some reason

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("guh")
 fetch("https://gachaytb3.org/addvisitor");
 
+// guh
 fetch("https://gachaytb3.org/visitorcount").then(d => {
         console.log(d);
         document.getElementById("visitors").innerText = "Visitors: " + d;
