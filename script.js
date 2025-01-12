90e8eb3022d0d88953a6c0eea20a1d59dd756cc6
3pm-on-github <pookiemon2019@gmail.com>
2025-01-12 10:54:07 +0100

    my rpi's script is outdated hlep

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 console.log("guh")
 fetch("https://gachaytb3.org/addvisitor");
 
-// guh
 fetch("https://gachaytb3.org/visitorcount").then(d => {
         console.log(d);
         document.getElementById("visitors").innerText = "Visitors: " + d;
